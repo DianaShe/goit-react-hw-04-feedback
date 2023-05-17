3c1f712ddeba0b85e5712270d4879f528723a319
DianaShe <dikravchyk@gmail.com>
2023-05-17 22:09:42 +0300

    Formatted code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,23 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Statistics from './Statistics/Statistics';
 
-export function App () {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const options = {good, neutral, bad}
+  const options = { good, neutral, bad };
 
   const handleClick = label => {
     switch (label) {
       case 'good':
-        setGood (prev => prev +1)
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral ((prev => prev +1))
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad ((prev => prev +1))
+        setBad(prev => prev + 1);
         break;
       default:
         break;
@@ -32,37 +32,31 @@ export function App () {
   };
 
   const countPositiveFeedbackPercentage = () => {
-    const positiveFeedbackCount =
-      (good / countTotalFeedback()) * 100;
+    const positiveFeedbackCount = (good / countTotalFeedback()) * 100;
     return Math.round(positiveFeedbackCount);
   };
 
-  
-    // const options = Object.keys(this.state);
-    
-
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(options)}
-            onLeaveFeedback={handleClick}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          {countTotalFeedback() > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={countTotalFeedback()}
-              positivePercentage={countPositiveFeedbackPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </>
-    );
-  
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(options)}
+          onLeaveFeedback={handleClick}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </>
+  );
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -20,9 +20,9 @@ export default function Statistics({
 }
 
 Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
-}
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
